5223042434e6b5866c3fdc018ad00a23dfb9ed43
Leonardo Muller <leonardomuller78@gmail.com>
2024-05-14 13:55:52 -0300

    feat: add route to create new task

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const connectToDataBase = require("./src/database/mongoose.database");
+const TaskModel = require("./src/models/task.model");
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 connectToDataBase();
 
-app.get("/tasks", (req, res) => {
-    const tasks = [{ description: "Estudar programação", isCompleted: false }];
-    res.status(200).send(tasks);
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await TaskModel.find({});
+        res.status(200).send(tasks);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.post("/tasks", async (req, res) => {
+    try {
+        const newTask = new TaskModel(req.body);
+
+        await newTask.save();
+
+        res.status(201).send(newTask);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 app.listen(8000, () => {
