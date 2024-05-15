739f4636bf506a10c730a5935eadadb13fc1395d
Leonardo Muller <leonardomuller78@gmail.com>
2024-05-14 22:12:26 -0300

    fetch: add cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const TaskRouter = require("./src/routes/task.routes");
+const cors = require("cors");
 
+const TaskRouter = require("./src/routes/task.routes");
 const connectToDataBase = require("./src/database/mongoose.database");
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 connectToDataBase();
